9ddbc40b59cd82305148f4ca2ddd4ddb12f25bbe
aobing <blue!Lemon!123>
2020-05-09 13:09:47 +0800

    [feat 05-09] redis

--- laogong/src/main/java/redis/RedisTest.java
+++ laogong/src/main/java/redis/RedisTest.java
@@ -6,6 +6,9 @@ import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 
+import java.awt.*;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.concurrent.*;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
@@ -17,31 +20,56 @@ import static java.util.concurrent.TimeUnit.SECONDS;
  **/
 public class RedisTest {
 
-    private static Integer inventory = 1001;
+    private static volatile Integer inventory = 1;
     private static final int NUM = 1000;
     private static LinkedBlockingQueue linkedBlockingQueue = new LinkedBlockingQueue();
+    volatile static CountDownLatch countDownLatch = new CountDownLatch(10);
+    static CyclicBarrier cyclicBarrier = new CyclicBarrier(5);
 
     public static void main(String[] args) {
-            ThreadPoolExecutor threadPoolExecutor =
-                    new ThreadPoolExecutor(inventory, inventory, 10L, SECONDS, linkedBlockingQueue);
-            long start = System.currentTimeMillis();
-            Config config = new Config();
-            config.useSingleServer().setAddress("redis://127.0.0.1:6379");
-            final RedissonClient client = Redisson.create(config);
-            final RLock lock = client.getLock("lock1");
-
-            for (int i = 0; i <= NUM; i++) {
-            threadPoolExecutor.execute(new Runnable() {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(;;){
+            new Thread(new Runnable() {
                 public void run() {
-                    lock.lock();
-                    inventory--;
-                    System.out.println(inventory);
-                    lock.unlock();
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        System.out.println("AAA");
+                        cyclicBarrier.await();
+                        System.out.println("BBB");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (BrokenBarrierException e) {
+                        e.printStackTrace();
+                    }
                 }
-            });
+            }).start();
         }
-        long end = System.currentTimeMillis();
-        System.out.println("执行线程数:" + NUM + "   总耗时:" + (end - start) + "  库存数为:" + inventory);
+
+
+//            ThreadPoolExecutor threadPoolExecutor =
+//                    new ThreadPoolExecutor(inventory, inventory, 10L, SECONDS, linkedBlockingQueue);
+//            long start = System.currentTimeMillis();
+//            Config config = new Config();
+//            config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+//            final RedissonClient client = Redisson.create(config);
+//            final RLock lock = client.getLock("lock1");
+//
+//            for (int i = 0; i <= NUM; i++) {
+//            threadPoolExecutor.execute(new Runnable() {
+//                public void run() {
+//                    lock.lock();
+//                    inventory--;
+//                    System.out.println(inventory);
+//                    lock.unlock();
+//                }
+//            });
+//        }
+//        long end = System.currentTimeMillis();
+//        System.out.println("执行线程数:" + NUM + "   总耗时:" + (end - start) + "  库存数为:" + inventory);
     }
 
 
