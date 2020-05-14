048ff749790c80a7ad617346721f6157e8d86690
aobing <blue!Lemon!123>
2020-05-14 11:53:44 +0800

    [feat 05-14] demo

--- laogong/src/main/java/redis/RedisBloomFilter.java
+++ laogong/src/main/java/redis/RedisBloomFilter.java
@@ -11,12 +11,9 @@ public class RedisBloomFilter {
                 1500,
                 0.01);
         // 判断指定元素是否存在
-        System.out.println(filter.mightContain(1));
-        System.out.println(filter.mightContain(2));
         // 将元素添加进布隆过滤器
         filter.put(1);
         filter.put(2);
-        System.out.println(filter.mightContain(1));
-        System.out.println(filter.mightContain(2));
+        filter.mightContain(1);
     }
 }
